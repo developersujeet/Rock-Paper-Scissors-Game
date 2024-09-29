7ba08e94e745f835bed1f98d58955bdbbd20a883
Developer Sujeet <157199958+developersujeet@users.noreply.github.com>
2024-09-29 14:34:52 +0530

    Update main.js

--- main.js
+++ main.js
@@ -11,39 +11,57 @@ const genCompChoice = () => {
     return options[rdmIdx];
 }
 const drawGame = () => {
-    Message.innerText = "Game was draw!";
+    Message.innerText = "Its a draw!";
     Message.style.backgroundColor = "#081b31";
+}
+const reGame = () => {
+    yourscore = 0;
+    compscore = 0;
+    Msg.style.visibility = "hidden";
+    updateDisplay();
+    Message.innerText = "Play your move"
+    Message.style.backgroundColor = "#081b31"
+    enableChoices();
+}
+const disableChoices = () => {
+    choices.forEach(choice => choice.style.pointerEvents = "none");
+};
+
+const enableChoices = () => {
+    choices.forEach(choice => choice.style.pointerEvents = "auto");
+};
+const updateDisplay = () => {
+    Yourscore.innerText = yourscore;
+    Compscore.innerText = compscore;
+    
 }
 const showWinner = (userWin,userChoice,compChoice) => {
     compChoice = compChoice.charAt(0).toUpperCase() + compChoice.slice(1);
     userChoice = userChoice.charAt(0).toUpperCase() + userChoice.slice(1);
     if (userWin) {
-    Message.innerText = `You win! ${userChoice} beats  ${compChoice}`;
+    Message.innerText = `You win! ${userChoice} beats ${compChoice}`;
     Message.style.backgroundColor = "green";
-    yourscore++
+    yourscore++;
+    
     if (yourscore === 10) {
-        Message.innerText = `You win! ${userChoice} beats  ${compChoice}`;
-        Msg.innerText = "Congratulations You won! Game Over."
-        Msg.style.backgroundColor = "green"
-        Msg.style.visibility = "visible"
-        Message.style.backgroundColor = "green";
-        yourscore = 0;
-        compscore = 0;
+        Msg.innerText = "Congratulations! You won! Game Over.";
+        Msg.style.backgroundColor = "green";
+        Msg.style.visibility = "visible";
+        disableChoices();
     }
 } else {
-    Message.innerText = `You lost! ${compChoice} beats  ${userChoice}`;
+    Message.innerText = `You lost! ${compChoice} beats ${userChoice}`;
     Message.style.backgroundColor = "red";
-    compscore++
+    compscore++;
+    
     if (compscore === 10) {
-        Message.innerText = `You Lost! ${compChoice} beats  ${userChoice}`;
-        Msg.innerText = "Game Over!"
-        Msg.style.backgroundColor = "red"
-        Msg.style.visibility = "visible"
-        Message.style.backgroundColor = "red";
-        yourscore = 0;
-        compscore = 0;
+        Msg.innerText = "Game Over.";
+        Msg.style.backgroundColor = "red";
+        Msg.style.visibility = "visible";
+        disableChoices();
     }
 }
+
 }
 const playGame = (userChoice) => {
     const compChoice = genCompChoice();
@@ -67,7 +85,6 @@ choices.forEach((choice) => {
     choice.addEventListener("click",() => {
         const userChoice = choice.getAttribute("id");
         playGame(userChoice);
-        Yourscore.innerText = yourscore;
-        Compscore.innerText = compscore;
+        updateDisplay();
     });
 });
